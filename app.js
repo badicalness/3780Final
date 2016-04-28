75687ed20ed15f5f308bb58c7f38c4ed66e43e20
badicalness <badicalness@gmail.com>
2016-04-27 22:43:18 -0600

    connected to firebase! Working on posting it.

--- app.js
+++ app.js
@@ -1,31 +1,66 @@
+angular.module('bomRadio', ["firebase"])
+    .controller('bomController', ['$scope', '$http', function($scope, $http) {
+        
+        var bomCtrl = this;
+        
+        bomCtrl.name = '';
+        $scope.type = '';
+        
+        $scope.bomStats = {
+            pages: 532,
+            chapters: 239,
+            verses: 1000,
+            
+        };
 
-
-
-var bomStats = {
-    pages: 532,
-    chapters: 239,
-    verses: 1000
+      
+      $scope.competion = function(type, days) {
+          console.log(type);
+          console.log($scope.daysToCompletion);
+          
+          if(type === 'Chapters') {
+              $scope.daysToCompletion = $scope.bomStats.chapters / days;
+              console.log($scope.daysToCompletion);
+          }
+          
+          if(type === 'Pages') {
+              $scope.daysToCompletion = $scope.bomStats.pages / days;
+              console.log($scope.daysToCompletion);
+          }
+          
+          if($scope.daysToCompletion) {
+              $scope.info = $scope.name + " needs to read " + $scope.daysToCompletion + " " + type + " per day."
+           } else {
+               $scope.info = "";
+           }
+          
+          $http.post("/", {info:$scope.info}).success(function(data, status){
+              console.log(data);
+              console.log(status);
+          });
+         
+          $http.get('/', function(data, status){
+              ref.once("value", function(data){
+              var info = snapshot.val();
+              res.send(info);
+          })
+           
+   
+//    $scope.sendInfo = function() {     
+//     $http.post('/', {'info': $scope.info})
+//         .then(function(res) {
+//           console.log('info saved');
+          
+//         })
+//     };
 }
+    // $scope.retrieveInfo = function() {
+    //   $http.get('/').then(function(res) {
+    //     console.log(res.data);
+    //     $scope.info = res.data;
+    //   });
+    // };
+    // $scope.retrieveinfo();
 
-
-
-  angular.module('bomRadio', [])
-    .controller('bomController', ['$scope', function($scope) {
-
-      
-      $scope.competion = function() {
-          console.log($scope.type);
-          
-          if($scope.type == 'Chapters') {
-              $scope.daysToCompletion = bomStats.chapters / $scope.days;
-              console.log($scope.daysToCompleteion)
-          }
-          
-          if($scope.type == 'Pages') {
-              $scope.daysToCompletion = bomStats.pages / $scope.days;
-              console.log($scope.daysToCompleteion)
-          }
-      }
-      
     }]);
 
--- server.js
+++ server.js
@@ -2,11 +2,28 @@ var express = require('express');
 var app = express();
 var Firebase = require('firebase');
 var ref = new Firebase('https://final3780bomapp.firebaseio.com/');
+var bodyParser = require('body-parser');
 
-ref.set({
-  {{type}}: "{{daysToCompletion}}",
+
+// app.use(express.static('source'));
+// app.use(express.static('node_modules/angularfire'));
+app.use(express.static('./'))
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({'extended': true}));
+
+app.get('/', function(req, res) {
+  ref.once("value", function(snapshot){
+    var info = snapshot.val();
+    res.send(info);
+  })
+
 });
 
-ref.child("{{daysToCompletion}}").on("value", function(snapshot) {
-  alert(snapshot.val()); 
+app.post('/', function(req, res) {
+  console.log(req.body.info);
+  ref.push({'info': req.body.info});
+  res.send('success!');
 });
+
+app.listen(8000);
+console.log('Server started on 8000');
