9e410568103b5488cae90e4ccbb2e051e6eae7db
badicalness <badicalness@gmail.com>
2016-04-28 12:12:03 -0600

    updating API

--- test api/app.js
+++ test api/app.js
@@ -1,17 +1,53 @@
 var express = require('express');
+    mongoose = require('mongoose');
+	bodyParser = require('body-parser');
+    
+var db = mongoose.connet('mongodb://localhost/bookAPI');
+
+var Book = require('/models/bookModel')
 
 var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
 var bookRouter = express.Router();
 
-bookRouter.route(/Books)
-    .get(function(req,res){
-        var responseJson = {hello: "This is still my practice API."};
+bookRouter.route('/Books')
+    .post(function(req,res){
+        var book = new Book(req.body);
         
-        res.json(responseJson);
-    }); 
+        console.log(book);
+        res.send(book);
+    })
+    
+    .get(function(req.res){
+        var query = {};
+        
+        if(req.query.genre)
+        {
+            query.genre = req.query.genre:
+        }
+        Book.find(query, function(err,books){
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(books);
+        });
+    });
+
+bookRouter.route('/Books/:bookId')
+    .get(function(req.res){
+        
+        Book.findById(req.params.bookId, function(err,book){
+            if(err)
+                res.status(500).send(err);
+            else
+                res.json(book);
+        });
+    });
     
 app.use('/api', bookRouter);
 
@@ -22,3 +58,7 @@ app.get('/', function(req, res) {
 app.listen(port, function() {
     console.log('Running on PORT: ' + port);
 });
+
+
+
+
---
+++ test api/models/bookModel.js
@@ -0,0 +1,13 @@
+var mongoos = require('mongoose'),
+Schema = mongoose.Schema();
+
+var bookModel = new Schema({
+    title: {
+        type: String
+    },
+    author: {type: String},
+    genre: {type: String},
+    read: {type: Boolean, default:false}
+});
+
+module.exports = mongoos.model('Book', bookModel);
